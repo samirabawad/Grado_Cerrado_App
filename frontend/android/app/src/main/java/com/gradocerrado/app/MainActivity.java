4baa455ef33ec1c197a59facc91722f5535591fa
gonza <gonza@DESKTOP-MV8TKLS>
2025-11-14 18:53:08 -0300

    acxtuializacion apk

--- frontend/android/app/src/main/java/com/gradocerrado/app/MainActivity.java
+++ frontend/android/app/src/main/java/com/gradocerrado/app/MainActivity.java
@@ -3,12 +3,12 @@ package com.gradocerrado.app;
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+//public class MainActivity extends BridgeActivity {
+  //  @Override
+   // public void onCreate(Bundle savedInstanceState) {
+   //     super.onCreate(savedInstanceState);
         
         // Registrar el plugin de Push Notifications
-        registerPlugin(com.capacitorjs.plugins.pushnotifications.PushNotificationsPlugin.class);
-    }
-}
+        //registerPlugin(com.capacitorjs.plugins.pushnotifications.PushNotificationsPlugin.class); 
+  //  }
+//}
