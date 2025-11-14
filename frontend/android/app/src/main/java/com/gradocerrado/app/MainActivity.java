8cdbb12f872648f12bc544c20e63211f967f00c9
gonza <gonza@DESKTOP-MV8TKLS>
2025-11-14 19:06:02 -0300

    cambios apk

--- frontend/android/app/src/main/java/com/gradocerrado/app/MainActivity.java
+++ frontend/android/app/src/main/java/com/gradocerrado/app/MainActivity.java
@@ -1,14 +1,5 @@
-package com.gradocerrado.app;
+package gradocerrado.app;
 
-import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-//public class MainActivity extends BridgeActivity {
-  //  @Override
-   // public void onCreate(Bundle savedInstanceState) {
-   //     super.onCreate(savedInstanceState);
-        
-        // Registrar el plugin de Push Notifications
-        //registerPlugin(com.capacitorjs.plugins.pushnotifications.PushNotificationsPlugin.class); 
-  //  }
-//}
+public class MainActivity extends BridgeActivity {}
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://gradocerrado-api-linux-agejfxg9d2ahgvgh.brazilsouth-01.azurewebsites.net/api'
+};export const environment = {
+  production: true,
+  apiUrl: 'https://gradocerrado-api-linux-agejfxg9d2ahgvgh.brazilsouth-01.azurewebsites.net/api'
 };
