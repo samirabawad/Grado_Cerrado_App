42314af2309fe49f7f36b84d03f6228a11f3bfb3
gonza <gonza@DESKTOP-MV8TKLS>
2025-11-14 20:11:33 -0300

    cambios apk

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,4 @@
 export const environment = {
   production: true,
   apiUrl: 'https://gradocerrado-api-linux-agejfxg9d2ahgvgh.brazilsouth-01.azurewebsites.net/api'
-};export const environment = {
-  production: true,
-  apiUrl: 'https://gradocerrado-api-linux-agejfxg9d2ahgvgh.brazilsouth-01.azurewebsites.net/api'
 };
